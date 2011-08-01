af38894baab7915c12d75e2418363b035e725da9
yigit sadic <yigitsadic@gmail.com>
2011-08-01 16:16:54 +0300

    index.html -> index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+	<table border="0" cellspacing="5" cellpadding="5" align="center">
+		<tr>
+			<th>
+				<h1>A</h1>
+			</th>
+			<th>
+				<h1>B</h1>
+			</th>			
+		</tr>
+		<tr>
+			<td>
+				<audio controls="controls">
+				  <source src="sub_test_wavs/cond_0/test_0.wav" type="audio/wav" />
+				</audio>
+			</td>
+
+			<td>				
+				<audio controls="controls">
+				  <source src="sub_test_wavs/cond_1/test_0.wav" type="audio/wav" />
+				</audio>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">
+				<button>-2</button>
+				<button>-1</button>
+				<button>0</button>
+				<button>+1</button>
+				<button>+2</button>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
